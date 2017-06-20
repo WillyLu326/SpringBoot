1f121a99f9b4c21bde542e845748edd66171e79e
WillyLu326 <luzh0326@gmail.com>
2017-06-20 19:56:33 -0400

    DI in spring boot

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,28 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
+    @Bean
+    public User user() {
+        return new User("Zheng", 28);
+    }
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName: beanNames) {
+            System.out.println(beanName);
+        }
 	}
 }
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,9 +3,6 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by zhenglu on 6/19/17.
- */
 
 @RestController
 public class HomeController {
