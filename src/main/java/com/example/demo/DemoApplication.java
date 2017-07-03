9021a0d714de7fd0b9d9e6df8adcb864808a6da9
WillyLu326 <luzh0326@gmail.com>
2017-07-02 23:26:24 -0400

    create indexController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,31 +1,14 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    @Bean
-    public User user() {
-        return new User("Zheng", 28);
-    }
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName: beanNames) {
-            System.out.println(beanName);
-        }
-
-        // name "user" comes from @Bean function
-        System.out.println(ctx.getBean("user"));
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
