b9e49d04e5050fc6a9523afc11a2edfee441f85f
WillyLu326 <luzh0326@gmail.com>
2017-07-02 23:45:13 -0400

    using @pathVariable

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.bean.User;
 
 @RestController
 @RequestMapping(value = "/index")
@@ -22,5 +26,14 @@ public class IndexController {
 		map.put("name", name);
 		map.put("greeting", "Hello World");
 		return map;
+	}
+	
+	@RequestMapping(value = "/find/{id}/{name}")
+	public User find(@PathVariable String id, @PathVariable String name) {
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setDate(new Date());
+		return user;
 	}
 }
