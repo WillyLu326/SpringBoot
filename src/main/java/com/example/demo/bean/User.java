314f386e3ed6c0e06b2f4b072067df4840f8e6e2
WillyLu326 <luzh0326@gmail.com>
2017-07-02 23:41:47 -0400

    create user bean

---
+++ src/main/java/com/example/demo/bean/User.java
@@ -0,0 +1,37 @@
+package com.example.demo.bean;
+
+import java.util.Date;
+
+public class User {
+
+	private String id;
+	
+	private String name;
+	
+	private Date date;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
