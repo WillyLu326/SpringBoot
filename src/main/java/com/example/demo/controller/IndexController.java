55e836da2fc715106b1a86c0b66cefd6d8a25014
WillyLu326 <luzh0326@gmail.com>
2017-07-02 23:38:46 -0400

    Using requestMapping

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/index")
 public class IndexController {
 
-	@RequestMapping("/")
+	@RequestMapping
 	public String index() {
 		return "Hello World";
+	}
+	
+	@RequestMapping(value = "/get")
+	public Map<String, String> get(@RequestParam String name) {
+		Map<String, String> map = new HashMap<>();
+		map.put("name", name);
+		map.put("greeting", "Hello World");
+		return map;
 	}
 }
