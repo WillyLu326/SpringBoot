66a96ce357b8861f51760df49e103ede664dcdab
WillyLu326 <luzh0326@gmail.com>
2017-07-10 22:03:54 -0400

    register CustomServlet as Bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.util.servlet.CustomServlet;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean() {
+		return new ServletRegistrationBean(new CustomServlet(), "/willy");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
