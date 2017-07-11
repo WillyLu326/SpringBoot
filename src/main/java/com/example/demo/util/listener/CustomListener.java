4f917fbb42c10c837efb5a86441e68d3eedcda1e
WillyLu326 <luzh0326@gmail.com>
2017-07-10 23:00:01 -0400

    Create CustomListenser implementing ServletContextListener

--- src/main/java/com/example/demo/util/listener/CustomListener.java
+++ src/main/java/com/example/demo/util/listener/CustomListener.java
@@ -1,5 +1,20 @@
 package com.example.demo.util.listener;
 
-public class CustomListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 
+public class CustomListener implements ServletContextListener{
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletContextListener Init");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		System.out.println("ServletContextListener Destroyed");
+	}
+	
 }
