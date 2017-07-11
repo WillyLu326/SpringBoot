6da40365aa5e4c493d7c12fb493e93d7198509c2
WillyLu326 <luzh0326@gmail.com>
2017-07-10 22:34:29 -0400

    register CustomFilter as Bean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+import com.example.demo.util.filter.CustomFilter;
 import com.example.demo.util.servlet.CustomServlet;
 
 @SpringBootApplication
@@ -13,6 +15,11 @@ public class DemoApplication {
 	@Bean
 	public ServletRegistrationBean servletRegistrationBean() {
 		return new ServletRegistrationBean(new CustomServlet(), "/willy");
+	}
+	
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		return new FilterRegistrationBean(new CustomFilter());
 	}
 	
 	public static void main(String[] args) {
