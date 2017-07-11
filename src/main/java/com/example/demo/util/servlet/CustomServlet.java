3386cffe33b457da4ab09eef6fffbf3c70d0107b
WillyLu326 <luzh0326@gmail.com>
2017-07-10 22:01:32 -0400

    extends HttpServlet and overid doGet function

--- src/main/java/com/example/demo/util/servlet/CustomServlet.java
+++ src/main/java/com/example/demo/util/servlet/CustomServlet.java
@@ -1,5 +1,26 @@
 package com.example.demo.util.servlet;
 
-public class CustomServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("sevlet GET Method");
+		resp.getWriter().write("hello world from doGet");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("sevlet POST Method");
+		super.doPost(req, resp);
+	}
 
 }
