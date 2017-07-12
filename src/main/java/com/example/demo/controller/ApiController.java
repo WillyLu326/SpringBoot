0e5415e8842e57803a471b5282319eaea0f1b4e9
WillyLu326 <luzh0326@gmail.com>
2017-07-11 22:23:58 -0400

    solve cors issue

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -3,8 +3,10 @@ package com.example.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class ApiController {
 
+	@CrossOrigin(origins = "http://localhost:8888", methods = RequestMethod.GET)
 	@RequestMapping(value = "/get")
 	public Map<String, String> get(@RequestParam String name) {
 		Map<String, String> map = new HashMap<>();
