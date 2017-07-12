efdbd602a4192af058c7a4823fac390772079d00
WillyLu326 <luzh0326@gmail.com>
2017-07-11 22:00:05 -0400

    using @WebFilter to inject filter

--- src/main/java/com/example/demo/util/filter/CustomFilter.java
+++ src/main/java/com/example/demo/util/filter/CustomFilter.java
@@ -8,7 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+@WebFilter(servletNames = "customServlet")
 public class CustomFilter implements Filter {
 
 	@Override
