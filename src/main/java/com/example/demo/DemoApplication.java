4955bfcb30c47241bee754174aea2e2750d49f1e
WillyLu326 <luzh0326@gmail.com>
2017-07-11 21:58:57 -0400

    using @WebServlet(urlPatterns = , name = ) to inject servlet

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,19 +1,10 @@
 package com.example.demo;
 
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-import com.example.demo.util.filter.CustomFilter;
-import com.example.demo.util.listener.CustomListener;
-import com.example.demo.util.servlet.CustomServlet;
-
+@ServletComponentScan
 @SpringBootApplication
 public class DemoApplication {
 	/**
