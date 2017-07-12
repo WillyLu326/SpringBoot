1a153543785d326a54ba8ea2fcda5bc2791b51ca
WillyLu326 <luzh0326@gmail.com>
2017-07-11 21:47:15 -0400

    Implement ServletContextInitializer to inject servlet filter and listener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,42 +1,46 @@
 package com.example.demo;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import com.example.demo.util.filter.CustomFilter;
-import com.example.demo.util.listener.CustomListener;
-import com.example.demo.util.servlet.CustomServlet;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements ServletContextInitializer{
+	
+	@Override
+	public void onStartup(ServletContext arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
-	@Bean
-	public ServletRegistrationBean servletRegistrationBean() {
-		return new ServletRegistrationBean(new CustomServlet(), "/willy");
-	}
-	
-	@Bean
-	public FilterRegistrationBean filterRegistrationBean() {
-		/**
-		 *  Filter all requests
-		 */
-		//return new FilterRegistrationBean(new CustomFilter());
-		/**
-		 *  Filter specific servlet
-		 */
-		return new FilterRegistrationBean(new CustomFilter(), servletRegistrationBean());
-	}
-	
-	@Bean
-	public ServletListenerRegistrationBean<CustomListener> servletListenerRegistrationBean() {
-		return new ServletListenerRegistrationBean<CustomListener>(new CustomListener());
-	}
+//	@Bean
+//	public ServletRegistrationBean servletRegistrationBean() {
+//		return new ServletRegistrationBean(new CustomServlet(), "/willy");
+//	}
+//	
+//	@Bean
+//	public FilterRegistrationBean filterRegistrationBean() {
+//		/**
+//		 *  Filter all requests
+//		 */
+//		//return new FilterRegistrationBean(new CustomFilter());
+//		/**
+//		 *  Filter specific servlet
+//		 */
+//		return new FilterRegistrationBean(new CustomFilter(), servletRegistrationBean());
+//	}
+//	
+//	@Bean
+//	public ServletListenerRegistrationBean<CustomListener> servletListenerRegistrationBean() {
+//		return new ServletListenerRegistrationBean<CustomListener>(new CustomListener());
+//	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
 }
