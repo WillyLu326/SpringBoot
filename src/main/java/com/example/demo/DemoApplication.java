4a7041aed358882615e7ba0f38f2149dee4ff02e
WillyLu326 <luzh0326@gmail.com>
2018-03-15 20:24:25 -0400

    clean project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,54 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class DemoApplication {
-	/**
-	 *  Second way to inject servlet, filter, listener. Willy Lu
-	 */
-	//implements ServletContextInitializer{
-	
-//	@Override
-//	public void onStartup(ServletContext servletContext) throws ServletException {
-//		// TODO Auto-generated method stub
-//		servletContext.addServlet("customServlet", CustomServlet.class).addMapping("/willylu");
-//		servletContext.addFilter("customFilter", CustomFilter.class)
-//			.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
-//			//.addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST), true, "customServlet");
-//		servletContext.addListener(new CustomListener());
-//	}
 
-	/**
-	 *  The first way to inject servlet filter listener
-	 */
-//	@Bean
-//	public ServletRegistrationBean servletRegistrationBean() {
-//		return new ServletRegistrationBean(new CustomServlet(), "/willy");
-//	}
-//	
-//	@Bean
-//	public FilterRegistrationBean filterRegistrationBean() {
-//		/**
-//		 *  Filter all requests
-//		 */
-//		//return new FilterRegistrationBean(new CustomFilter());
-//		/**
-//		 *  Filter specific servlet
-//		 */
-//		return new FilterRegistrationBean(new CustomFilter(), servletRegistrationBean());
-//	}
-//	
-//	@Bean
-//	public ServletListenerRegistrationBean<CustomListener> servletListenerRegistrationBean() {
-//		return new ServletListenerRegistrationBean<CustomListener>(new CustomListener());
-//	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
 }
--- src/main/resources/templates/index.ftl
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>FreeMaker in SpringBoot</title>
-</head>
-<body>
-	<h1>This is FreeMaker in Srping Boot</h1>
-</body>
-</html>
