c633a9cddfe1d47bab19554adf88c6a7f0b7c701
WillyLu326 <luzh0326@gmail.com>
2018-03-16 21:17:20 -0400

    created Models

--- src/main/java/com/example/services/NotificationService.java
+++ src/main/java/com/example/services/NotificationService.java
@@ -1,5 +1,11 @@
 package com.example.services;
 
+import java.util.List;
+
+import com.example.model.Author;
+
 public interface NotificationService {
 
+	public List<Author> lists();
+	
 }
--- src/main/java/com/example/services/helpers/NotificationServiceHelper.java
+++ src/main/java/com/example/services/helpers/NotificationServiceHelper.java
@@ -1,7 +1,16 @@
 package com.example.services.helpers;
 
+import java.util.List;
+
+import com.example.model.Author;
 import com.example.services.NotificationService;
 
 public class NotificationServiceHelper implements NotificationService {
+
+	@Override
+	public List<Author> lists() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
