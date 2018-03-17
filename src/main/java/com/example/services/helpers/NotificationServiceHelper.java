eedc095df2d409d66bcfdadbc59decc3886bc6fc
WillyLu326 <luzh0326@gmail.com>
2018-03-16 21:14:52 -0400

    update names

---
+++ src/main/java/com/example/services/helpers/NotificationServiceHelper.java
@@ -0,0 +1,7 @@
+package com.example.services.helpers;
+
+import com.example.services.NotificationService;
+
+public class NotificationServiceHelper implements NotificationService {
+
+}
--- src/main/java/com/example/services/impl/NotificationServiceImplementation.java
+++
@@ -1,7 +0,0 @@
-package com.example.services.impl;
-
-import com.example.services.NotificationService;
-
-public class NotificationServiceImplementation implements NotificationService {
-
-}
