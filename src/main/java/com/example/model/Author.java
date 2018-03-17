f95d3dee5e8ebc73be3e8fcc4e6cea6bbfacbbf0
WillyLu326 <luzh0326@gmail.com>
2018-03-16 21:21:08 -0400

    create Author

--- src/main/java/com/example/model/Author.java
+++ src/main/java/com/example/model/Author.java
@@ -2,18 +2,35 @@ package com.example.model;
 
 public class Author {
 
+	private long id;
 	private String name;
 	private int age;
 	
+	public Author() {}
+	
+	public Author(long id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public int getAge() {
 		return age;
 	}
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
--- src/main/java/com/example/services/helpers/NotificationServiceHelper.java
+++ src/main/java/com/example/services/helpers/NotificationServiceHelper.java
@@ -7,10 +7,19 @@ import com.example.services.NotificationService;
 
 public class NotificationServiceHelper implements NotificationService {
 
+	private List<Author> authors;
+	
+	public NotificationServiceHelper() {
+		Author one = new Author(1, "Willy Lu", 29);
+		Author two = new Author(2, "Bingjie Xu", 28);
+		
+		this.authors.add(one);
+		this.authors.add(two);
+	}
+	
 	@Override
 	public List<Author> lists() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.authors;
 	}
 
 }
